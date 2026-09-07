bdbea6ff401738e765f384c2b5756df896f0c141
David Zbarsky <dzbarsky@gmail.com>
2026-09-07 15:53:18 -0400

    Bound Bazel memory for LLVM prebuilt releases

--- .github/workflows/llvm-prebuilt.sh
+++ .github/workflows/llvm-prebuilt.sh
@@ -33,10 +33,13 @@ fi
 
 bazel \
   --bazelrc=".github/workflows/ci.bazelrc" \
+  --host_jvm_args=-Xmx8g \
   build \
   --remote_header=x-buildbuddy-api-key=4jtaxdhxtyu4ylxdEwI7 \
   --config=remote \
   --config=release \
+  --jobs=128 \
+  --discard_analysis_cache \
   --repo_env=BAZEL_MSVC_RUNTIME_VISUAL_STUDIO_EULA=1 \
   --repo_env=BAZEL_WINDOWS_SDK_EULA=1 \
   --remote_download_outputs=toplevel \
